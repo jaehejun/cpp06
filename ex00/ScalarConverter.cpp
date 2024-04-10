95858231e12ebecf3c71763994999b8531ef4356
Jaehee Jung <wogml414@gmail.com>
2024-04-10 22:10:32 +0900

    scalar converter...

---
+++ ex00/ScalarConverter.cpp
@@ -0,0 +1,20 @@
+#include "ScalarConverter.hpp"
+
+void ScalarConverter::convert(const std::string &param)
+{
+	// detect the type of the param
+	// convert it from string to actual type(char, int, float, double)
+	// 
+
+	std::cout << char
+	std::cout << int
+	std::cout << float
+	std::cout << double
+
+	if (char)
+	else if (int)
+	else if (float)
+	else if (double)
+
+	std::cout << param << std::endl;
+}
